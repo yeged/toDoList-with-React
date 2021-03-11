35d3204967e1cc23d6ae5e21f5905ebc514d51a7
yeged <yigityceylan@gmail.com>
2021-03-11 20:00:10 +0300

    Get names from LocalStorage & Add Font Awesome & Style

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,10 +2,18 @@ import React, { useState, useCallback } from "react"
 import CreateComponent from "./CreateComponent/CreateComponent"
 import CardComponent from "./CardComponent/CardComponent"
 import Information from "./InformationComponent/InformationComponent"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core"
+
+library.add(faUser)
 
 const Main = () => {
 
     const [check, setCheck] = useState(true)
+
+    const fname = JSON.parse(localStorage.getItem('names')).fname
+    const lname = JSON.parse(localStorage.getItem('names')).lname
 
 
     //Confirm Names
@@ -16,15 +24,32 @@ const Main = () => {
 
     return (
         <div>
+            {check ? <Information onClick={namesHandler} /> : <div className="container">
+                <div className="row">
+                    <div className="col col-md-3">
+                        <div className="row-content">
+                            <div className="row">
+                                <div className="col col-md col-lg-8" style={{ border: "1px solid black", height: "150px", borderRadius: "60%", backgroundColor: "lightgrey", overflow: "hidden" }}>
+                                    <FontAwesomeIcon style={{ margin: "20px 0px" }} icon="user" size="8x" />
+                                </div>
+                                <div className="col col-md col-lg-4">
+                                    <h4>{fname + " " + lname}</h4>
+                                </div>
 
-            {check ? <Information onClick={namesHandler} /> :
-                <div>
-                    <CreateComponent />
-                </div>}
-
-
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col col-md-9">
+                        <div className="row">
+ 
+                            <div className="col col-md-4">
+                                <CreateComponent />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>}
         </div>
-
     )
 }
 
